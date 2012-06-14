4d527f8d5bba705ce14be7b4926363f3524ad78f
Tyler Wymer <twymer@gmail.com>
2012-06-14 13:05:56 -0400

    Finally committing the bash script I run when installing this

---
+++ bin/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.ackrc ~/.ackrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zsh ~/.zsh
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
