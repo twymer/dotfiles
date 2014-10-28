b541e0ccf80e834eb3a7a8828f0f7d011f0d8085
Tyler Wymer <twymer@gmail.com>
2014-10-28 14:09:13 -0400

    Tweak starthq script

--- bin/starthq.sh
+++ bin/starthq.sh
@@ -3,6 +3,7 @@
   tell application "iTerm"
     set myterm to (make new terminal)
     set cdhqdir to "cd ~/code/dimagi/commcare-hq"
+    set workonhq to "workon hq_env15"
     tell myterm
       launch session "Default session"
       tell the last session
@@ -14,15 +15,23 @@
       launch session "Default session"
       tell the last session
         set name to "editor"
+        write text "workonhq"
         write text cdhqdir
         write text "vim"
       end tell
 
       launch session "Default session"
       tell the last session
+        set name to "tests"
+        write text cdhqdir
+        write text "workonhq"
+      end tell
+
+      launch session "Default session"
+      tell the last session
         set name to "server"
         write text cdhqdir
-        write text "sourcehq"
+        write text "workonhq"
         write text "./manage.py runserver"
       end tell
 
@@ -36,7 +45,7 @@
       tell the last session
         set name to "ptop"
         write text cdhqdir
-        write text "sourcehq"
+        write text "workonhq"
         # write text "./manage.py run_ptop --all"
       end tell
 
@@ -44,7 +53,7 @@
       tell the last session
         set name to "celery"
         write text cdhqdir
-        write text "sourcehq"
+        write text "workonhq"
         write text "./manage.py celeryd -P gevent --verbosity=2 --beat --statedb=celery.db --events"
       end tell
 
