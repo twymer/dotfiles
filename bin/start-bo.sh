27a9c4e02230a601778e42fa012bd5e64c715464
Tyler Wymer <twymer@dimagi.com>
2015-06-11 23:44:51 -0400

    Add script to be lazy for now

---
+++ bin/start-bo.sh
@@ -0,0 +1,46 @@
+ #!/bin/sh
+  osascript <<-eof
+  tell application "iTerm"
+    set myterm to (make new terminal)
+    set bodir to "cd ~/code/dbc/tech/backoffice && clear"
+    set ssodir to "cd ~/code/dbc/tech/sso && clear"
+
+    tell myterm
+      launch session "Default session"
+      tell the last session
+        set name to "editor"
+        write text bodir
+        write text "vim"
+      end tell
+
+      launch session "Default session"
+      tell the last session
+        set name to "tests"
+        write text bodir
+      end tell
+
+      launch session "Default session"
+      tell the last session
+        set name to "backoffice"
+        write text bodir
+        write text "powder link"
+        write text "powder applog"
+      end tell
+
+      launch session "Default session"
+      tell the last session
+        set name to "sso"
+        write text ssodir
+        write text "powder link"
+      end tell
+
+      launch session "Default session"
+      tell the last session
+        set name to "redis"
+        write text bodir
+        write text "redis-server"
+      end tell
+
+    end tell
+  end tell
+eof
