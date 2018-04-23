6ee56b2793440dd823871bc73a27c278925b632f
Tyler Wymer <twymer@gmail.com>
2018-04-23 14:13:47 -0500

    Refactor cedar scripts

--- bin/cedar-scripts.sh
+++ bin/cedar-scripts.sh
@@ -1,70 +1,88 @@
-function _nukedb() {
-  docker-compose run cedar-api ./manage.py nukedb
+function manage_api {
+  # Pass anything called after `manage_api` along to manage.py.
+  # Example:
+  # $ manage_api nukedb
+  docker-compose run cedar-api ./manage.py "$@"
 }
 
-function _migrate() {
-  docker-compose run cedar-api ./manage.py migrate
+function fast_reset_db {
+  docker-compose kill cedar-db
+  docker-compose rm cedar-db
+  docker-compose up -d cedar-db
 }
 
-function _fixtures() {
-  docker-compose run cedar-api ./manage.py loaddata api_app/fixtures/initial.json
-}
-
-function _reset() {
-  _nukedb
+function fast_reset_seed {
+  fast_reset_db
   _migrate
   _fixtures
+  _success "Cedar DB is reset and seeded"
 }
 
-function reset-db() {
-  _reset
-  osascript -e 'display notification "Cedar DB is ready" with title "Done" sound name "Hero"'
-}
-
-function _seed() {
-  docker-compose run cedar-api ./manage.py loaddata automated_ui_tests.json
-  # docker-compose run cedar-api ./manage.py shell < /usr/local/cedar/api/api_app/scripts/create-automated-ui-test-patients.py
-}
-
-
-function seed-tests() {
+function seed_tests {
   _seed
-  osascript -e 'display notification "Cedar DB is fully seeded" with title "Done" sound name "Hero"'
+  _success "Cedar DB is fully seeded"
 }
 
-function reset-db-full() {
+function reset_db_full {
   _reset
   _seed
-  osascript -e 'display notification "Cedar DB is reset and seeded" with title "Done" sound name "Hero"'
+  _success "Cedar DB is reset and seeded"
 }
 
-function restart-channels() {
+function restart_channels {
   docker-compose exec cedar-worker-channels bash -c "supervisorctl restart channels-worker"
   docker-compose exec cedar-worker-celery bash -c "supervisorctl restart celery-worker"
 }
 
-function restart-supervisor() {
+function restart_supervisor {
   docker-compose exec cedar-api bash -c "supervisorctl restart all"
 }
 
-function restart() {
+function restart {
   restart-channels
   restart-supervisor
-  osascript -e 'display notification "Cedar app is done restarting" with title "Done" sound name "Hero"'
+  _success "Cedar app is done restarting"
 }
 
-function cedar-exports() {
+function _success {
+  osascript -e "display notification \"$1\" with title \"Done\" sound name \"Hero\""
+}
+
+function cedar_exports {
   export CEDAR_SERVICES_ACTIVE=true
   export HOSTNAME=`hostname`
   export AWS_CREDENTIAL_HOME=~
 }
 
-function makemessages() {
+function makemessages {
   django-admin makemessages -l=es
-  osascript -e 'display notification "makemessages command complete" with title "Done" sound name "Hero"'
+  _success "makemessages command complete"
 }
 
-function compilemessages() {
+function compilemessages {
   django-admin compilemessages
-  osascript -e 'display notification "compilemessages command complete" with title "Done" sound name "Hero"'
+  _success "compilemessages command complete"
+}
+
+function _reset {
+  _nukedb
+  _migrate
+  _fixtures
+}
+
+function _seed {
+  docker-compose run cedar-api ./manage.py loaddata automated_ui_tests.json
+  # docker-compose run cedar-api ./manage.py shell < /usr/local/cedar/api/api_app/scripts/create-automated-ui-test-patients.py
+}
+
+function _nukedb {
+  docker-compose run cedar-api ./manage.py nukedb
+}
+
+function _migrate {
+  docker-compose run cedar-api ./manage.py migrate
+}
+
+function _fixtures {
+  docker-compose run cedar-api ./manage.py loaddata api_app/fixtures/initial.json
 }
